3c3d6ae679ce5fe6a408b9538cb299617980fbf2
bhaskar <bhaskarraochandhaka@gmail.com>
2026-06-20 12:57:50 +0530

    update

--- main.tf
+++ main.tf
@@ -14,11 +14,11 @@ resource "aws_internet_gateway" "gw" {
 
 }
 # pulic subnets 
-resource "public_subnet" "public" {
+resource "aws_subnet" "public" {
   count              = length(var.public_subnet_cidrs)
   vpc_id             = aws_vpc.main.id
   cidr_block         = var.public_subnet_cidrs[count.index]
-  availability_zones = local.az_names[count.index]
+  availability_zone = local.az_names[count.index]
   map_public_ip_on_launch = true
 
   tags = merge(
